df4df04a9164c23bb60f7e14ba4123c2806b2f18
midoandbodyemish <computermbwe@gmail.com>
2025-03-31 15:10:04 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,23 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.11.1/firebase-messaging.js');
 
-const firebaseConfig = {
+importScripts("https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging.js");
+
+firebase.initializeApp({
     apiKey: "AIzaSyAJzK9G2MJieXHmeeHUBdULdDt8NwyxyKQ",
     authDomain: "khedma-fd534.firebaseapp.com",
     projectId: "khedma-fd534",
-    storageBucket: "khedma-fd534.appspot.com",
+    storageBucket: "khedma-fd534.firebasestorage.app",
     messagingSenderId: "186556101962",
     appId: "1:186556101962:web:3edb278b50948378f83db3",
     measurementId: "G-F6C0180ZDS"
-};
+});
 
-firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-    console.log("📩 إشعار وارد في الخلفية: ", payload);
+    console.log("📩 إشعار مستلم في الخلفية:", payload);
     self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
-        icon: "https://k.top4top.io/p_3169bgwkb0.jpg"
+        icon: "/icon.png"
     });
 });
